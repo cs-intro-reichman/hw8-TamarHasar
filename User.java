a225680ecf9f93da133e6fe576ad6b64d4c9939a
TamarHasar <tamar.hasar@post.runi.ac.il>
2025-01-07 09:29:20 +0200

    fixed user class

--- User.java
+++ User.java
@@ -43,6 +43,7 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
+        name = name.toLowerCase();
         for (int i = 0; i < fCount; i++) {
             if (follows[i].equals(name)) {
                 return true;
@@ -53,6 +54,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
+        name = name.toLowerCase();
         if (follows(name) || fCount == maxfCount){
             return false;
         }
